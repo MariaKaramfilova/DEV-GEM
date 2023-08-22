eaf7c448dd6f2f0fb7f07380da537c048a4b223d
maria_karamfilova <karamfilova.mgeorgieva@gmail.com>
2023-08-22 18:27:35 +0300

    Add basic MUI setup and basic homepage

---
+++ src/views/Header/Header.tsx
@@ -0,0 +1,52 @@
+import { AppBar, Toolbar, Typography, IconButton, Button, Avatar, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react'
+
+type Props = {}
+
+export default function Header({ }: Props) {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleMenuOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          My Website
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Services</Button>
+        <Button color="inherit">Contact</Button>
+        <IconButton color="inherit" onClick={handleMenuOpen}>
+          <Avatar alt="Profile" src="/path-to-profile-image.jpg" />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Settings</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+}
