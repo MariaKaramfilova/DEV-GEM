c1d4fd6e3119b1a8293fccced305ea69f1d97abd
Hristiyan Fachikov <hristiyan.fachikov>
2023-08-24 21:36:22 +0300

    Fixing small things

--- src/components/Addons-Library/Addons.tsx
+++ src/components/Addons-Library/Addons.tsx
@@ -119,10 +119,14 @@ export default function AddonCard() {
   return (
     <>
       <div className="addon-group">
+      {topRatings.length > 0 ? (
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <h2 style={{ color: "black", textAlign: "left", marginLeft: "30px" }}>Top Downloads</h2>
           <Button style={{ marginRight: "30px", marginTop: "20px" }} onClick={() => handleViewMore("top-downloads")}>View more</Button>
         </div>
+         ) : (
+            <h2 style={{ color: "black", textAlign: "left", marginLeft: "30px" }}>Top Downloads</h2>
+        )}
         {topDownloads.length > 0 ? (
           <div className="addon-card-grid">
             {topDownloads.map((addon) => (
@@ -130,15 +134,21 @@ export default function AddonCard() {
             ))}
           </div>
         ) : (
-          <p style={{ color: "black" }}>{MESSAGE_FOR_TOP_DOWNLOAD_ADDONS}</p>
+          <p style={{ color: "gray" }}>{MESSAGE_FOR_TOP_DOWNLOAD_ADDONS}</p>
         )}
       </div>
 
       <div className="addon-group">
+      {topNewAddons.length > 0 ? (
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <h2 style={{ color: "black", marginTop: "70px", textAlign: "left", marginLeft: "30px" }}>Top Related</h2>
           <Button style={{ marginRight: "30px", marginTop: "60px"}} onClick={() => handleViewMore("top-related")}>View more</Button>
         </div>
+         ):(
+          <>
+          <h2 style={{ color: "black", marginTop: "70px", textAlign: "left", marginLeft: "30px" }}>Top Related</h2>
+          </>
+        )}
         {topRatings.length > 0 ? (
           <div className="addon-card-grid">
             {topRatings.map((addon) => (
@@ -146,15 +156,21 @@ export default function AddonCard() {
             ))}
           </div>
         ) : (
-          <p style={{ color: "black" }}>{MESSAGE_FOR_TOP_RELATED_ADDONS}</p>
+          <p style={{ color: "gray" }}>{MESSAGE_FOR_TOP_RELATED_ADDONS}</p>
         )}
       </div>
 
       <div className="addon-group">
+      {topNewAddons.length > 0 ? (
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <h2 style={{ color: "black", marginTop: "70px", textAlign: "left", marginLeft: "30px" }}>New Addonis</h2>
           <Button style={{ marginRight: "30px", marginTop: "60px" }} onClick={() => handleViewMore("new-addons")}>View more</Button>
         </div>
+        ):(
+          <>
+            <h2 style={{ color: "black", marginTop: "70px", textAlign: "left", marginLeft: "30px" }}>New Addonis</h2>
+          </>
+        )}
         {topNewAddons.length > 0 ? (
           <div className="addon-card-grid">
             {topNewAddons.map((addon) => (
@@ -162,7 +178,7 @@ export default function AddonCard() {
             ))}
           </div>
         ) : (
-          <p style={{ color: "black" }}>{MESSAGE_FOR_NEW_ADDONS}</p>
+          <p style={{ color: "gray" }}>{MESSAGE_FOR_NEW_ADDONS}</p>
         )}
       </div>
     </>
