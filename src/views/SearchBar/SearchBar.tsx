81d69f1125b3c7bf0ce05bb2af5a3d84fc39d799
viktor.mp <viktor.martinov.p@gmail.com>
2023-09-01 01:34:01 +0300

    someCss

--- src/views/SearchBar/SearchBar.tsx
+++ src/views/SearchBar/SearchBar.tsx
@@ -13,9 +13,9 @@ const SearchBar = () => {
 
   return (
     <>
-      <div style={{ color: "black" }}>
-        <h1 style={{ fontSize: '34px', fontWeight: 'normal', marginBottom: '25px', fontFamily: 'Roboto, sans-serif' }}>Find extensions to build an <br />app quickly and easily</h1>
-        <h3 style={{ fontSize: '15px', fontWeight: 'normal', marginBottom: '25px', color: 'gray', fontFamily: 'Roboto, sans-serif' }}>Personalize your developer experience with CrafterKit Marketplace plugins adding even more features to your favorite CrafterKit IDE and team tools.</h3>
+      <div style={{ color: "black"}}>
+        <h1 style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '25px', fontFamily: 'Roboto, sans-serif' }}>Find extensions to build an <br />app quickly and easily</h1>
+        <h3 style={{ fontSize: '18px', fontWeight: 'normal', marginBottom: '25px', color: 'grey', fontFamily: 'Roboto, sans-serif' }}>Personalize your developer experience with CrafterKit Marketplace plugins adding even more features to your favorite CrafterKit IDE and team tools.</h3>
       </div>
       <div className="search-bar-container">
         <div className="search-bar" >
