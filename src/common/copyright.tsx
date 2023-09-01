0b416429ef092fccc518aa3ee78924c2cb913739
maria_karamfilova <karamfilova.mgeorgieva@gmail.com>
2023-09-01 20:12:41 +0300

    Added footer styles

--- src/common/copyright.tsx
+++ src/common/copyright.tsx
@@ -9,9 +9,7 @@ function Copyright(props: Props): JSX.Element {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Unknown Addonis
-      </Link>{' '}
+        DEV GEM &nbsp;
       {new Date().getFullYear()}
       {'.'}
     </Typography>
