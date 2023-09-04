675480fb47d021a2a8e5c516f3737df5c403c183
maria_karamfilova <karamfilova.mgeorgieva@gmail.com>
2023-09-04 19:46:05 +0300

    Small changes to order view

--- src/components/Checkout/OrderReview.tsx
+++ src/components/Checkout/OrderReview.tsx
@@ -1,58 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
-
-const products = [
-  {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '$9.99',
-  },
-  {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '$3.45',
-  },
-  {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '$6.51',
-  },
-  {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '$14.11',
-  },
-  { name: 'Shipping', desc: '', price: 'Free' },
-];
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
-const payments = [
-  { name: 'Card type', detail: 'Visa' },
-  { name: 'Card holder', detail: 'Mr John Smith' },
-  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-  { name: 'Expiry date', detail: '04/2024' },
-];
+import { useParams } from 'react-router';
+import { getAddonById } from '../../services/addon.services.ts';
+import { Alert, Snackbar } from '@mui/material';
 
 export default function OrderReview() {
+  const [addon, setAddon] = useState('');
+  const [error, setError] = useState(null);
+  const params = useParams();
+  const addonId = params.addon;
+
+  console.log(addon);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (addonId) {
+          const response = await getAddonById(addonId);
+          setAddon(response);
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    })();
+  }, [addonId]);
+
+  if (error) {
+    return (
+      <Snackbar open={true} autoHideDuration={2000} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+        <Alert severity="error">The following error has ocurred: {error}</Alert>
+      </Snackbar>
+    )
+  }
+
   return (
     <Fragment>
       <Typography variant="h6" gutterBottom>
         Order summary
       </Typography>
       <List disablePadding>
-        {products.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
-          </ListItem>
-        ))}
+        <ListItem key={addonId} sx={{ py: 1, px: 0 }}>
+          <ListItemText primary={addon.name} />
+        </ListItem>
         <ListItem sx={{ py: 1, px: 0 }}>
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $34.06
+            ${addon.price}
           </Typography>
         </ListItem>
       </List>
@@ -62,14 +59,14 @@ export default function OrderReview() {
             Shipping
           </Typography>
           <Typography gutterBottom>John Smith</Typography>
-          <Typography gutterBottom>{addresses.join(', ')}</Typography>
+          {/* <Typography gutterBottom>{addresses.join(', ')}</Typography> */}
         </Grid>
         <Grid item container direction="column" xs={12} sm={6}>
           <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
             Payment details
           </Typography>
           <Grid container>
-            {payments.map((payment) => (
+            {/* {payments.map((payment) => (
               <Fragment key={payment.name}>
                 <Grid item xs={6}>
                   <Typography gutterBottom>{payment.name}</Typography>
@@ -78,7 +75,7 @@ export default function OrderReview() {
                   <Typography gutterBottom>{payment.detail}</Typography>
                 </Grid>
               </Fragment>
-            ))}
+            ))} */}
           </Grid>
         </Grid>
       </Grid>
