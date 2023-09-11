f26930cc11e90800be468f944e6c7a4a6c8bd991
maria_karamfilova <karamfilova.mgeorgieva@gmail.com>
2023-09-11 19:18:53 +0300

    Footer is set at the bottom of the page

--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -7,43 +7,57 @@ import Copyright from '../../common/copyright';
 import { Link } from '@mui/joy';
 
 export default function StickyFooter() {
+  const [isPageLong, setIsPageLong] = useState<boolean>();
+
+  React.useEffect(() => {
+    if (document.body.offsetHeight > window.innerHeight) {
+      setIsPageLong(true);
+    } else {
+      setIsPageLong(false);
+    }
+  }, []);
+
   return (
 
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        position: "absolute",
+        width: "100%",
+        left: 0,
+        right: 0,
+        margin: "4em 0 0 0",
+        overflow: "hidden",
+        top: "auto",
+        padding: 0,
+        bottom: isPageLong ? "auto" : 0,
+      }}
+    >
+      <CssBaseline />
+
       <Box
+        component="footer"
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          position: "absolute",
-          width: "100%",
-          left: 0, 
-          right: 0, 
-          margin: "4em 0 0 0",
+          py: 3,
+          px: 2,
+          padding: "2em",
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
         }}
       >
-        <CssBaseline />
-        
-        <Box
-          component="footer"
-          sx={{
-            py: 3,
-            px: 2,
-            padding: "2em",
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[200]
-                : theme.palette.grey[800],
-          }}
-        >
-          <Container maxWidth="md">
-            <Typography variant="body1">
+        <Container maxWidth="md">
+          <Typography variant="body1">
             This application was made as an educational project. All content is not intended for real-world use and any transactions are in test mode. Please refrain from using the content in any other way.
             <br />
             <Link href="https://gitlab.com/f-ep-group/addonis">GitHub repo link</Link>
-            </Typography>
-            <Copyright />
-          </Container>
-        </Box>
+          </Typography>
+          <Copyright />
+        </Container>
       </Box>
+    </Box>
 
   );
 }
