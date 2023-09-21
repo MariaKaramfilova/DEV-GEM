a745faadc83648841edafd924a6a42bc8f61cdc9
maria_karamfilova <karamfilova.mgeorgieva@gmail.com>
2023-09-21 16:01:04 +0300

    Login test added

--- tests/pages/login.test.tsx
+++ tests/pages/login.test.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
 import Login from '../../src/components/Login/Login.tsx';
 import { BrowserRouter as Router } from "react-router-dom";
@@ -10,11 +11,10 @@ import { LOG_IN_PATH } from '../../src/common/common.ts';
 describe('Login component tests', () => {
 
   it('Renders correctly initial document', async () => {
-    /* first we visit /login and test if the string in the element with class "login-label"  has"Please Log In" is there */
     render(
       <Router>
         <Routes>
-        <Route path={LOG_IN_PATH} element={<Login />} />
+          <Route path={LOG_IN_PATH} element={<Login />} />
         </Routes>
         <Login />
       </Router>
@@ -22,6 +22,32 @@ describe('Login component tests', () => {
     const loginLabel = screen.getByText('Sign In');
 
     expect(loginLabel).toBeInTheDocument();
+  });
+
+  it('Displays error for wrong input', async () => {
+    render(
+      <Router>
+        <Routes>
+          <Route path={LOG_IN_PATH} element={<Login />} />
+        </Routes>
+        <Login />
+      </Router>
+    );
+
+    const emailInput = screen.getByLabelText(/email address/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    fireEvent.change(emailInput, { target: { value: 't@t.com' } });
+    fireEvent.change(passwordInput, { target: { value: '555' } });
+
+    const submitButton = screen.getByRole('button', { name: /sign in/i });
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const errorMessage = screen.queryByText('An error occurred. Please try again later.');
+      expect(errorMessage).toBeInTheDocument();
+    });
+
   });
 
 });
